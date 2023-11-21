a3069507a113f1e4c34678bd421b353f1b9a68b6
Brainmated <alsynetos@hotmail.com>
2023-11-21 16:27:17 +0200

    Update

--- abstract/django/weather_project/weather_app/views.py
+++ abstract/django/weather_project/weather_app/views.py
@@ -32,18 +32,15 @@ def index(request):
         return render(request, "weather_app/index.html")
     
 def fetch_weather_and_forecast(city, api_key, current_weather_url, forecast_url):
-    #send to the api and get responses
-    response = requests.get(current_weather_url.format(city, api_key)).json() #as a dictionary
-    lat, lon = response["coord"]["lat"], response["coord"]["lon"]
-    forecast_response = requests.get(forecast_url.format(lat, lon, api_key)).json
-
-    #format to be readable
+    response = requests.get(current_weather_url.format(city, api_key)).json()
+    lat, lon = response['coord']['lat'], response['coord']['lon']
+    forecast_response = requests.get(forecast_url.format(lat, lon, api_key)).json()
 
     weather_data = {
-        "city": city,
-        "temperature": round(response["main"]["temp"]),
-        "description": response["weather"][0]["description"],
-        "icon": response["weather"][0]["icon"]
+        'city': city,
+        'temperature': round(response['main']['temp'] - 273.15, 2),
+        'description': response['weather'][0]['description'],
+        'icon': response['weather'][0]['icon'],
     }
     #a list to append each forecast
     daily_forecasts = []
