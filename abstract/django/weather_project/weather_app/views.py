1aeeb1af935a77b642a60a40c5420972f417ecc7
Brainmated <alsynetos@hotmail.com>
2023-11-21 16:28:58 +0200

    Api fault

--- abstract/django/weather_project/weather_app/views.py
+++ abstract/django/weather_project/weather_app/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    API_KEY = open("E:/Programming in Python/applications/Thesis/abstract/django/weather_project/API_KEY", "r").read()
+    api_key = open("E:/Programming in Python/applications/Thesis/abstract/django/weather_project/API_KEY", "r").read()
     current_weather_url = "https://api.openweathermap.org/data/2.5/weather?q={}&appid={}"
     forecast_url = "https://api.openweathermap.org/data/2.5/onecall?lat={}&lon={}&exclude=current,minutely,hourly,alerts&appid={}"
 
