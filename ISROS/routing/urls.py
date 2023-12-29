b5813aa1191a9f85e6a65a2a8b9e82621668a04e
Brainmated <alsynetos@hotmail.com>
2023-12-29 10:43:53 +0200

    pathing to urls

---
+++ ISROS/routing/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import index, login_view, logout_view
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
+    # Add other paths as needed
+]
--- ISROS/routing/views.py
+++ ISROS/routing/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth import logout
+from django.shortcuts import redirect
 
-# Create your views here.
+
+# Home page view
+def index(request):
+    return render(requestIt seems the previous response was cut off. Let's continue from where we left off.
+
+### Step 6: Configure URLs and Views
+
+Define the views for each page in `routing/views.py`:
+
+```python
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth import logout, login, authenticate
+from django.contrib.auth.forms import AuthenticationForm
+
+# Home page view
+def index(request):
+    return render(request, 'index.html')
+
+# Login view
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            if user:
+                login(request, user)
+                return redirect('index')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
+# Logout view
+def logout_view(request):
+    logout(request)
+    return render(request, 'logout.html')
