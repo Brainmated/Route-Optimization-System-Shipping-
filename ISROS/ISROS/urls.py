8bb9eec9ea5f321965fa52ea851f245bf2ad6867
Brainmated <alsynetos@hotmail.com>
2023-12-29 10:30:39 +0200

    set index

--- ISROS/ISROS/urls.py
+++ ISROS/ISROS/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('shipping/', include('interface.urls')),
+    path('', views.index, name='index')
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
