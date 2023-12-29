156e1fd84fe53ed01a0dbedc485fce29ade37efc
Brainmated <alsynetos@hotmail.com>
2023-12-29 14:07:33 +0200

    end of day 29

--- ISROS/routing/urls.py
+++ ISROS/routing/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import index, login_view, logout_view
+from .views import index, login_view, logout_view, show_map
 from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
@@ -8,5 +8,6 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', LogoutView.as_view(next_page='index'), name='logout'),
     path('signup/', views.signup, name='signup'),
+    path('map/', show_map, name='map'),
     # Add other paths as needed
 ]
