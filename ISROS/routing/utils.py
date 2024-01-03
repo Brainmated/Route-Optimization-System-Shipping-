752004ac19d3255faf3c8b89d339fe14374caec8
Brainmated <alsynetos@hotmail.com>
2024-01-03 12:40:34 +0200

    final changes

--- ISROS/routing/utils.py
+++ ISROS/routing/utils.py
@@ -5,8 +5,18 @@ def get_ports_from_csv(csv_filepath):
     with open(csv_filepath, newline='') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
-            coordinates = (row['Latitude'], row['Longitude'])
+            lat = row['Latitude']
+            lon = row['Longitude']
             port_name = row['Main Port Name']
-            
-            ports.append((port_name, coordinates))
+            # Ensure that latitude and longitude are converted from string to float
+            try:
+                lat_float = float(lat)
+                lon_float = float(lon)
+            except ValueError as e:
+                # Handle the exception if conversion fails
+                print(f"Could not convert lat/lon to float for port {port_name}: {e}")
+                continue  # Skip this row and move to the next
+
+            # Append a tuple with all necessary information
+            ports.append((port_name, lat_float, lon_float))
     return ports
