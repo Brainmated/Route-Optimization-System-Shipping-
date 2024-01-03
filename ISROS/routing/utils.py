a1a74218571b5fd01b8774136941cf01c2da76fa
Brainmated <alsynetos@hotmail.com>
2024-01-03 12:37:17 +0200

    changed parsing, updated map handling on map.html

--- ISROS/routing/utils.py
+++ ISROS/routing/utils.py
@@ -5,18 +5,8 @@ def get_ports_from_csv(csv_filepath):
     with open(csv_filepath, newline='') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
-            lat = row['Latitude']
-            lon = row['Longitude']
+            coordinates = (row['Latitude'], row['Longitude'])
             port_name = row['Main Port Name']
-            # Ensure that latitude and longitude are converted from string to float
-            try:
-                lat_float = float(lat)
-                lon_float = float(lon)
-            except ValueError as e:
-                # Handle the exception if conversion fails
-                print(f"Could not convert lat/lon to float for port {port_name}: {e}")
-                continue  # Skip this row and move to the next
-
-            # Append a tuple with all necessary information
-            ports.append((port_name, lat_float, lon_float))
+            
+            ports.append((port_name, coordinates))
     return ports
