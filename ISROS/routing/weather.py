e9bd9b320683a023c5ab85f4da4b34ceb695cc37
Brainmated <alsynetos@hotmail.com>
2024-02-10 13:33:29 +0200

    get_weather_data method in weather.py

--- ISROS/routing/weather.py
+++ ISROS/routing/weather.py
@@ -1,3 +1,26 @@
 import requests
 from config import API_KEY
 from pathing import get_path_coordinates
+
+STORM_GLASS_API_ENDPOINT = 'https://api.stormglass.io/v2/weather/point'
+
+def get_weather_data(latitude, longitude):
+    params =v{
+        "lat":latitude,
+        "lon":longitude,
+        "params": ",".join(["airTemperature", "windSpeed", "waveheight"]),
+        #check if anything else should be added
+    }
+
+    headers = {
+        "Authorization": API_KEY
+    }
+
+    response = requests.get(STORM_GLASS_API_ENDPOINT, params=params, headers=headers)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Error for data: ({latitude}, {longitude})")
+        return None
+    
