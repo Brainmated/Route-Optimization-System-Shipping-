b8737e4043b3137b73924cc499bb5472cbdf919d
Brainmated <alsynetos@hotmail.com>
2024-02-10 13:45:27 +0200

    will test weather.py on 11/2

--- ISROS/routing/weather.py
+++ ISROS/routing/weather.py
@@ -24,3 +24,27 @@ def get_weather_data(latitude, longitude):
         print(f"Error for data: ({latitude}, {longitude})")
         return None
     
+def generate_weather_map(path_coordinates):
+    weather_data = []
+
+    for coord in path_coordinates:
+        data = get_weather_data(coord["latitude"], coord["longitude"])
+
+        if data:
+            weather_data.append({
+                "lat":coord["latitude"],
+                "lon": coord["longitude"],
+                "airTemperature": data["hours"][0]["airTemperature"]["noaa"],
+                "windSpeed": data["hours"][0]["windSpeed"]["noaa"],
+                "waveHeight": data["hours"][0]["waveHeight"]["noaa"],
+                #maybe something else, i dont know
+            })
+    #Integrate the data in the debug.html, find a way
+    #just print for now
+    for item in weather_data:
+        print(item)
+    
+    #if get_path_coordinates from pathing.py return coordinates like:
+    #[{"latitude": 59.91, "longitude":10.75}, "latitude": 59.92, "longitude":10.76}]
+    path_coordinates = get_path_coordinates()
+    generate_weather_map(path_coordinates)
