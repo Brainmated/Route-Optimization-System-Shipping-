6e51870112e8d6ec59adbb0d072797a359066501
Alex Synetos Konstantinidis <93430275+Brainmated@users.noreply.github.com>
2024-03-19 18:32:47 +0200

    Update views.py

--- ISROS/routing/views.py
+++ ISROS/routing/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import messages
 from django.urls import reverse
 from .forms import SignUpForm
-from .pathing import Pathing, GridMap, Map_Marking
+from .pathing import a_star, dijkstra, G
 from django.contrib.auth.views import LoginView, LogoutView
 from .utils import get_ports_from_csv
 import folium
@@ -89,35 +89,6 @@ def debug_view(request):
     folium.PolyLine([(0, -180), (0, 180)], color="red", weight=0.3).add_to(m)  # Equator
     folium.PolyLine([(-90, 0), (90, 0)], color="red", weight=0.3).add_to(m)  # Prime Meridian
 
-    '''Generate two random locations and draw a line between them
-    random_points = []
-    feature_collection = {"type": "FeatureCollection", "features": []}
-    for _ in range(2):
-        random_lat = random.uniform(-90, 90)
-        random_lon = random.uniform(-180, 180)
-        random_points.append((random_lat, random_lon))  # Append the point to the list
-        folium.Marker([random_lat, random_lon]).add_to(m)  # Create a marker for the random location
-    
-    # Draw a line between the two random points
-    folium.PolyLine(random_points, color="blue", weight=2.5, opacity=0.8).add_to(m)
-    '''
-
-    '''--------Coastline Test------------------
-    for coords in coast_coords:
-        folium.PolyLine(coords, color="red", weight=2.5, opacity=0.8).add_to(m)
-    
-    # Create a GeoJSON feature for the point
-    feature = {
-        "type": "Feature",
-        "geometry": {
-            "type": "Point",
-            "coordinates": [random_lon, random_lat],
-        },
-        "properties": {},
-    }
-    feature_collection["features"].append(feature)
-    '''
-
     # Render map to HTML
     init_map_html = m._repr_html_()
 
@@ -135,7 +106,6 @@ def simulate(request):
     min_lat, max_lat = -90, 90 
     min_lon, max_lon = -180, 180 
     grid_size = 1
-    grid_map = GridMap(distance_threshold=5) #test with 5 km
 
     m = folium.Map(
     location=[0, 0],
@@ -155,8 +125,6 @@ def simulate(request):
     for lon in range(-180, 180, grid_size):
         folium.PolyLine([(-90, lon), (90, lon)], color="blue", weight=0.1).add_to(m)
 
-    #obtaining the straight_path
-    m, formatted_distance = Pathing.straight_path(request, m)
 
     # Extract location A and B from the POST data
     loc_a_name = request.POST.get("locationA")
@@ -169,8 +137,8 @@ def simulate(request):
     if loc_a is None or loc_b is None:
         return JsonResponse({"error": "One or both locations not found."}, status=400)
 
-    start_node = grid_map.get_nearest_walkable_node(float(loc_a['latitude']), float(loc_a['longitude']))
-    goal_node = grid_map.get_nearest_walkable_node(float(loc_b['latitude']), float(loc_b['longitude']))
+    start_node = (float(loc_a['longitude']), float(loc_a['latitude']))
+    goal_node = (float(loc_b['longitude']), float(loc_b['latitude']))
 
     # Check if start_node and goal_node are returned correctly
     if start_node is None:
@@ -179,23 +147,14 @@ def simulate(request):
     if goal_node is None:
         return JsonResponse({"error": "Goal location is not walkable or not found."}, status=400)
 
-    #obtaining the a_star path -------------------------------------------------------------------------------------------------------FIX
-    #result = Pathing.a_star(request, grid_map)
-    grid_map.initialize_map(start_node, goal_node)
-    pathing_instance = Pathing(grid_map)
+
     try:
-        a_star_path = pathing_instance.a_star(start_node, goal_node)
+        a_star_path = a_star(G, start_node, goal_node)
+        folium.PolyLine(a_star_path, color="green", weight=2, opacity=1).add_to(m)
 
+    except Exception as e:
+        return JsonResponse({"error": str(e)}, status=500)
 
-        if a_star_path:
-            #Extract coordinates
-            a_star_path_coords = [(node.lat, node.lon) for node in a_star_path]
-            #Draw the path with folium
-            folium.PolyLine(a_star_path_coords, color="green", weight=2, opacity=1).add_to(m)
-        else:
-            print("No path found using A* algorithm")
-    except ValueError as e:
-        print(f"Error with A* algorithm: {e}")
 
     # Serialize the map to HTML
     map_html = m._repr_html_()
@@ -203,7 +162,7 @@ def simulate(request):
     context = {
         "map_html": map_html,
         "simulation_run": True,
-        "distance_km": formatted_distance,
+        "distance_km": "Test",
         "locationA": request.POST.get("locationA"),
         "locationB": request.POST.get("locationB"),
     }
